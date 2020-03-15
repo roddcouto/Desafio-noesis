f84d8b227c4e5e56170bf0d618675e299aa1eabf
roddcouto <roddcouto@gmail.com>
2020-03-15 17:10:49 -0300

    Atualização do Projeto Unimed-Selenium

--- Unimed-Selenium/features/step_definitions/cenario_dois_stepdefs.js
+++
@@ -1,5 +0,0 @@
-const assert = require('assert');
-const { Given, When, Then } = require('cucumber');
-const { Builder, By, Key, until } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-
