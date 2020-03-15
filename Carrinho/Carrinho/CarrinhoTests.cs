323ea118f266434c69167f9d54cd6124c9f72207
roddcouto <roddcouto@gmail.com>
2020-03-14 23:01:29 -0300

    Desafio 3 - TDD Desconto Lojista

---
+++ Carrinho/Carrinho/CarrinhoTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carrinho
+{
+  [TestFixture]
+  class CarrinhoTests
+  {
+    [Test]
+    public void AcimaDe100EAbaixode200()
+    {
+
+    }
+
+    [Test]
+    public void AcimaDe200EAbaixode300()
+    {
+
+    }
+
+    [Test]
+    public void AcimaDe300EAbaixode400()
+    {
+
+    }
+
+    [Test]
+    public void AcimaDe400()
+    {
+
+    }
+  }
+}
---
+++ Carrinho/Carrinho/Filme.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Carrinho
+{
+  public class Filme
+  {
+    
+    public int id { get; set; }
+    public string nome { get; set; }
+    public string genero { get; set; }
+    public double preco { get; set; }
+    
+
+   
+
+  
+
+    public Filme()
+    {
+      
+    }
+  }
+}
